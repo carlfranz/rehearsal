50577d4c21525d1e115230cbbad563f75f3684ab
Carlo <pioniere@gmail.com>
2016-03-17 13:18:53 +0100

    running template
    application running template

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector:'my-app',
+    template:`
+        <h1>Rehearsal</h1>
+    `
+
+})
+
+export class AppComponent {
+          
+    
+    
+}
