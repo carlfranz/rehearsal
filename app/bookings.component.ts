4786f0fa246eac7d0fe8ca070dc8da03d8245c7a
Carlo <pioniere@gmail.com>
2016-03-18 14:29:43 +0100

    aggiuno spinner al caricamento

--- app/bookings.component.ts
+++ app/bookings.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from 'angular2/core';
 import { Observable } from 'rxjs/Rx';
 import {HTTP_PROVIDERS} from 'angular2/http'
+
 import { BookingService } from './bookings.service';
+import { SpinnerComponent } from './spinner.component';
 
 @Component({
     selector: 'bookings',
@@ -9,10 +11,11 @@ import { BookingService } from './bookings.service';
     providers : [BookingService,HTTP_PROVIDERS],
     styles :[`
         .list-group-item:hover {
-            background-color: blue;
+            background-color: #f5f5f5;
             cursor: default;
         }
-    `]
+    `],
+    directives: [SpinnerComponent]
     
     
 })
@@ -20,14 +23,21 @@ import { BookingService } from './bookings.service';
 export class BookingsComponent implements OnInit {
 
     rooms; 
+    selectedRoom;
+    isLoading = true;
     
     constructor(private _bookingService: BookingService) { }
     
+    roomSelected(room) {
+        this.selectedRoom = room; 
+    }
  
     ngOnInit() { 
         this._bookingService.getBookings()
             .subscribe(
-                data => {this.rooms = data}
+                data => {this.rooms = data},
+                null,
+                () => {this.isLoading = false}
             );
         
     }
